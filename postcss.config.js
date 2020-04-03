f0858cd16f3dde504ff47532273ea671514253d6
wenyuanzhuo <775416111@qq.com>
2020-04-03 17:04:57 +0800

    fix: postcss browserslist waining

--- postcss.config.js
+++ postcss.config.js
@@ -5,12 +5,6 @@ module.exports = {
     },
     'postcss-url': {},
     'autoprefixer': {
-      browsers: [
-        '>1%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9', // React doesn't support IE8 anyway
-      ],
       flexbox: 'no-2009',
     },
     'postcss-aspect-ratio-mini': {},
